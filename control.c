70ee0a8009f72ec7007534a0528cb8ad2082897e
Alvin Zhu <alvinzhu33@yahoo.com>
2016-12-08 15:30:42 -0500

    Worked on control

--- control.c
+++ control.c
@@ -5,3 +5,33 @@
 #include <sys/sem.h>
 #include <string.h>
 #include "semunion.h"
+
+int main(int argc, char *argv[]){
+  //printf("%d\n", argc);
+  //printf("%s\n", argv[1]);
+
+  if(argc == 1){
+    printf("Choose an action:\nCreate: -c\nRemove: -r\nView: -v\n");
+  }else{
+    int semid;
+    int key = ftok("makefile", 22);
+    int sc;
+    
+    if(strcmp(argv[1], "-c") == 0){
+      //printf("-c\n");
+      int semid = semget(key, 1, IPC_CREAT | 0644);
+
+      if(semid >= 0 ){
+	union semun su;
+	
+      }
+    }
+    if(strcmp(argv[1], "-r") == 0){
+      printf("-r\n");
+    }
+    if(strcmp(argv[1], "-v") == 0){
+      printf("-v\n");
+    }
+  }
+  return 0;
+}
--- semunion.h
+++ semunion.h
@@ -3,4 +3,4 @@ union semun {
   struct semid_ds *buf;    /* Buffer for IPC_STAT, IPC_SET */
   unsigned short  *array;  /* Array for GETALL, SETALL */
   struct seminfo  *__buf;  /* Buffer for IPC_INFO*/
-}
+};
