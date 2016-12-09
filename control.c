58ab03d561ded1fca5c94eada652c5252ab65d2c
Karen Huang <khuang428@gmail.com>
2016-12-09 15:17:45 -0500

    things

--- control.c
+++ control.c
@@ -1,9 +1,3 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <sys/types.h>
-#include <sys/ipc.h>
-#include <sys/sem.h>
-#include <string.h>
 #include "semunion.h"
 
 int main(int argc, char *argv[]){
@@ -13,25 +7,38 @@ int main(int argc, char *argv[]){
   if(argc == 1){
     printf("Choose an action:\nCreate: -c\nRemove: -r\nView: -v\n");
   }else{
-    int semid;
-    int key = ftok("makefile", 22);
+    int semkey;
     int sc;
+    int semid;
+    int fd;
+    int fsize;
+    int shmkey;
+    int shmid;
     
     if(strcmp(argv[1], "-c") == 0){
       //printf("-c\n");
-      int semid = semget(key, 1, IPC_CREAT | 0644);
+      semkey = ftok("makefile", 22);
+      sc;
+      semid = semget(semkey, 1, IPC_CREAT | 0644);
 
-      if(semid >= 0 ){
-	union semun su;
-	
-      }
+      fd = open("semaphone.ring", O_CREAT | O_TRUNC | 0644);
+      fsize = lseek(fd, 0, SEEK_END);
+      //printf("size: %d\n", fsize);
+      
+      shmkey = ftok("makefile", 23);
+      shmid = shmget(shmkey, fsize, IPC_CREAT | 0644);
     }
+    
     if(strcmp(argv[1], "-r") == 0){
-      printf("-r\n");
+      //printf("-r\n");
+      
     }
+    
     if(strcmp(argv[1], "-v") == 0){
-      printf("-v\n");
+      //printf("-v\n");
+      
     }
   }
+  close(fd);
   return 0;
 }
--- semunion.h
+++ semunion.h
@@ -1,3 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/sem.h>
+#include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
+
 union semun {
   int              val;    /* Value for SETVAL */
   struct semid_ds *buf;    /* Buffer for IPC_STAT, IPC_SET */
