dcc22045e26904134997fcd7b0aa9421c9d46393
Live session user <khuang428@gmail.com>
2016-12-10 06:03:42 +0000

    finished control

--- control.c
+++ control.c
@@ -7,45 +7,52 @@ int main(int argc, char *argv[]){
   if(argc == 1){
     printf("Choose an action:\nCreate: -c\nRemove: -r\nView: -v\n");
   }else{
-    int semkey;
+    int semkey = ftok("makefile", 22);
     int sc;
     int semid;
+    int shmkey = ftok("makefile", 23);
+    int shmid;
     int fd;
     int fsize;
-    int shmkey;
-    int shmid;
-    char * buffer;
-    
+ 
     if(strcmp(argv[1], "-c") == 0){
       //printf("-c\n");
-      semkey = ftok("makefile", 22);
-      sc;
       semid = semget(semkey, 1, IPC_CREAT | 0644);
+
+      shmid = shmget(shmkey, fsize, IPC_CREAT | 0644);
 
       fd = open("semaphone.ring", O_CREAT | O_TRUNC | 0644);
       fsize = lseek(fd, 0, SEEK_END);
       //printf("size: %d\n", fsize);
-      
-      shmkey = ftok("makefile", 23);
-      shmid = shmget(shmkey, fsize, IPC_CREAT | 0644);
       close(fd);
     }
     
     if(strcmp(argv[1], "-r") == 0){
       //printf("-r\n");
+      fd = open("semaphone.ring", O_RDONLY);
+      fsize = lseek(fd, 0, SEEK_END);
+      lseek(fd,-1 * fsize, SEEK_CUR);
+      char buffer[fsize];
+      read(fd, buffer, fsize);
+      //printf("size: %d\n", fsize);
+      printf("%s\n", buffer);
+      close(fd);
+
       semctl(semkey, 0, IPC_RMID);
 
-      fd = open("semaphone.ring", O_RDONLY | 0644);
-      fsize = lseek(fd, 0, SEEK_END);
-      read(fd, buffer, fsize);
-      printf("size: %d\n", fsize);
-      printf("Buffer: %s\n", buffer);
-      close(fd);
+      shmctl(shmkey, IPC_RMID, 0);
     }
     
     if(strcmp(argv[1], "-v") == 0){
       //printf("-v\n");
-      
+      fd = open("semaphone.ring", O_RDONLY);
+      fsize = lseek(fd, 0, SEEK_END);
+      lseek(fd,-1 * fsize, SEEK_CUR);
+      char buffer[fsize];
+      read(fd, buffer, fsize);
+      //printf("size: %d\n", fsize);
+      printf("%s\n", buffer);
+      close(fd); 
     }
   }
   return 0;
--- semunion.h
+++ semunion.h
@@ -3,6 +3,7 @@
 #include <sys/types.h>
 #include <sys/ipc.h>
 #include <sys/sem.h>
+#include <sys/shm.h>
 #include <string.h>
 #include <fcntl.h>
 #include <unistd.h>
