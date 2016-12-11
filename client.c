52c93398a70e6855f667936bbba1c0496813cb13
Alvin Zhu <alvinzhu33@yahoo.com>
2016-12-11 13:10:39 -0500

    progress?

--- client.c
+++ client.c
@@ -7,8 +7,21 @@
 #include "semunion.h"
 
 void main(){
-  int semid;
-  int shmid;
-  semid = semget(ftok("makefile", 22), 1, 0644);
-  shmid = shmget(ftok("makefile", 23), 0, 0644);
+    int key = ftok("makefile", 22);
+    int semid = semget(key, 1, 0644);
+    int shmid = shmget(key, 4, 0644);
+    int fd = open("semaphone.ring", O_RDWR | O_APPEND | 0644);
+
+    struct sembuf sb;
+    sb.sem_num = 0;
+    sb.sem_flg = SEM_UNDO;
+    sb.sem_op = -1;
+    semop(semid, &sb, 1);
+
+    int *size = (int *)shmat(shmid, 0, 0);
+    char text[*size + 1];
+    lseek(fd, -(*size), SEEK_END);
+    read(fd, text, *size);
+    close(fd);
+    printf("Last entry: %s\n", text);
 }
--- control.c
+++ control.c
@@ -7,26 +7,28 @@ int main(int argc, char *argv[]){
   if(argc == 1){
     printf("Choose an action:\nCreate: -c\nRemove: -r\nView: -v\n");
   }else{
-    int semkey = ftok("makefile", 22);
+    int key = ftok("makefile", 22);
     int sc;
     int semid;
-    int shmkey = ftok("makefile", 23);
     int shmid;
     int fd;
     int fsize;
- 
+    union semun su;
+
     if(strcmp(argv[1], "-c") == 0){
       //printf("-c\n");
-      semid = semget(semkey, 1, IPC_CREAT | 0644);
+      semid = semget(key, 1, IPC_CREAT | 0644);
+      su.val = 1;
+      semctl(semid, 0, SETVAL, su);
 
-      shmid = shmget(shmkey, fsize, IPC_CREAT | 0644);
+      shmid = shmget(key, 4, IPC_CREAT | 0644);
 
       fd = open("semaphone.ring", O_CREAT | O_TRUNC | 0644);
       fsize = lseek(fd, 0, SEEK_END);
       //printf("size: %d\n", fsize);
       close(fd);
     }
-    
+
     if(strcmp(argv[1], "-r") == 0){
       //printf("-r\n");
       fd = open("semaphone.ring", O_RDONLY);
@@ -38,11 +40,13 @@ int main(int argc, char *argv[]){
       printf("%s\n", buffer);
       close(fd);
 
-      semctl(semkey, 0, IPC_RMID);
+      semid = semget(key, 1, 0);
+      semctl(key, 0, IPC_RMID);
 
-      shmctl(shmkey, IPC_RMID, 0);
+      shmid = shmget(key, 4, 0);
+      shmctl(key, IPC_RMID, 0);
     }
-    
+
     if(strcmp(argv[1], "-v") == 0){
       //printf("-v\n");
       fd = open("semaphone.ring", O_RDONLY);
@@ -52,7 +56,7 @@ int main(int argc, char *argv[]){
       read(fd, buffer, fsize);
       //printf("size: %d\n", fsize);
       printf("%s\n", buffer);
-      close(fd); 
+      close(fd);
     }
   }
   return 0;
