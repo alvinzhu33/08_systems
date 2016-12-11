e5caa2b27ffb7f071cd81559b3078eb7903024f5
Alvin Zhu <alvinzhu33@yahoo.com>
2016-12-11 14:09:23 -0500

    LEGIT PROGRESS!

--- client.c
+++ client.c
@@ -10,7 +10,7 @@ void main(){
     int key = ftok("makefile", 22);
     int semid = semget(key, 1, 0644);
     int shmid = shmget(key, 4, 0644);
-    int fd = open("semaphone.ring", O_RDWR | O_APPEND | 0644);
+    int fd = open("semaphone.ring", O_APPEND | O_RDWR | 0666);
 
     struct sembuf sb;
     sb.sem_num = 0;
@@ -18,10 +18,22 @@ void main(){
     sb.sem_op = -1;
     semop(semid, &sb, 1);
 
-    int *size = (int *)shmat(shmid, 0, 0);
+    int *size;
+    size = (int *) shmat(shmid, 0, 0);
+    printf("Size: %d\n", *size);
     char text[*size + 1];
     lseek(fd, -(*size), SEEK_END);
     read(fd, text, *size);
-    close(fd);
+    text[*size]=0;
     printf("Last entry: %s\n", text);
+
+    char next[256];
+    printf("Next entry: ");
+    fgets(next, sizeof(next), stdin);
+    write(fd, next, strlen(next));
+
+    sb.sem_op = 1;
+    semop(semid, &sb, 1);
+
+    close(fd);
 }
